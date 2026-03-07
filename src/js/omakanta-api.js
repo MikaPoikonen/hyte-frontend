8fa8231278bf4a8d39ab041aa67e9d871c5de8b2
MikaPoikonen <mikapoi@metropolia.fi>
2026-03-07 20:53:46 +0200

    final

--- src/js/omakanta-api.js
+++ src/js/omakanta-api.js
@@ -1,4 +1,4 @@
-// Omakanta API -kutsut
+// Omakanta API -kutsut oman id haku
 export async function getMyId() {
   //const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats');
 
@@ -37,7 +37,7 @@ export async function getMyId() {
 
 
 
-
+// Tietokannasta tapahtumien haku
 export async function getOmakantaStats() {
   //const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats');
 
@@ -67,42 +67,118 @@ export async function getOmakantaStats() {
   return res.json();
 }
 
-
+// Päiväkirjamerkinnän lisääminen
 export async function postDailyStats(payload) {
-const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats');
+  const token = localStorage.getItem('token');
+  console.log(token);
 
-let headers = {};
-let token = localStorage.getItem('token');
-console.log(token);
-if (token) {
-headers = { Authorization: `Bearer ${token}`,
-'content-type': 'application/json',
-}
-	
-const options = {
-    method : 'POST',
-		headers: headers,
+  if (!token) {
+    throw new Error('Token puuttuu');
+  }
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+
+  const options = {
+    method: 'POST',
+    headers: headers,
     body: JSON.stringify(payload),
-	};
-  const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats', options );
-  if (!res.ok) {
-    throw new Error(`Stats haku epäonnistui: ${res.status}`);
+  };
 
+  const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats', options);
+
+  if (!res.ok) {
+    const text = await res.text();
+    console.log('POST error body:', text);
+    throw new Error(`Stats lisäys epäonnistui: ${res.status}`);
   }
 
   const contentType = res.headers.get('content-type') || '';
   if (!contentType.includes('application/json')) {
     const text = await res.text();
-    console.log(text)
-    ;
+    console.log(text);
     throw new Error(`API ei palauttanut JSONia. Alku: ${text.slice(0, 80)}`);
   }
 
-  return res.json();
+  return await res.json();
 }
 
+
+// Päiväkirjamerkinnän päivittäminen
+export async function putDailyStats(payload) {
+  let token = localStorage.getItem('token');
+  console.log(token);
+
+  if (!token) {
+    throw new Error('Token puuttuu');
+  }
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+
+  const options = {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify(payload),
+  };
+
+  const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats', options);
+
+  if (!res.ok) {
+    throw new Error(`Stats päivitys epäonnistui: ${res.status}`);
+  }
+
+  const contentType = res.headers.get('content-type') || '';
+  if (!contentType.includes('application/json')) {
+    const text = await res.text();
+    console.log(text);
+    throw new Error(`API ei palauttanut JSONia. Alku: ${text.slice(0, 80)}`);
+  }
+
+  return await res.json();
 }
 
+
+
+// Päiväkirjamerkinnän päivittäminen
+export async function deleteDailyStat(payload) {
+  let token = localStorage.getItem('token');
+  console.log(token);
+
+  if (!token) {
+    throw new Error('Token puuttuu');
+  }
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+
+  const options = {
+    method: 'DELETE',
+    headers: headers,
+    body: JSON.stringify(payload),
+  };
+
+  const res = await fetch('http://127.0.0.1:3000/api/omakanta/stats', options);
+
+  if (!res.ok) {
+    throw new Error(`Stats päivitys epäonnistui: ${res.status}`);
+  }
+
+  const contentType = res.headers.get('content-type') || '';
+  if (!contentType.includes('application/json')) {
+    const text = await res.text();
+    console.log(text);
+    throw new Error(`API ei palauttanut JSONia. Alku: ${text.slice(0, 80)}`);
+  }
+
+  return await res.json();
+}
 
 
 
--- src/js/paivakirja.js
+++ src/js/paivakirja.js
@@ -1,12 +1,18 @@
-import { getOmakantaStats,postDailyStats,getMyId } from './omakanta-api.js';
+import {
+  getOmakantaStats,
+  postDailyStats,
+  getMyId,
+  putDailyStats,
+  deleteDailyStat,
+} from "./omakanta-api.js";
 import "../css/omakanta.css";
 
 // Haetaan localstoragesta nimi
-let name = localStorage.getItem('name');
-document.querySelector('.username').textContent = name ? name : 'vieras'; //
-
+let name = localStorage.getItem("name");
+document.querySelector(".username").textContent = name ? name : "vieras"; //
 
 //Päivämäärän muokkaus oikeeseen muotoon json varten
+// Päivämäärä ja kerronaika
 function formatDate(iso) {
   const d = new Date(iso);
   return d.toLocaleString("fi-FI", {
@@ -14,9 +20,11 @@ function formatDate(iso) {
     month: "2-digit",
     year: "numeric",
     hour: "2-digit",
-    minute: "2-digit"
+    minute: "2-digit",
   });
 }
+
+// Formatoidaan päivämäärä ilman kellonaikaa
 function formatDateClock(iso) {
   const d = new Date(iso);
   return d.toLocaleString("fi-FI", {
@@ -26,24 +34,22 @@ function formatDateClock(iso) {
   });
 }
 
-const diaryForm = document.querySelector('#add-text-site');
-const diaryTextField = document.querySelector('#text-ul');
-const myDialog = document.querySelector('#myDialog');
-const dialogIdEl = document.querySelector('#myDialog #dialogId');
-const totalStesUl = document.querySelector('#stats-ul');
+const diaryForm = document.querySelector("#add-text-site");
+const diaryTextField = document.querySelector("#text-ul");
+const myDialog = document.querySelector("#myDialog");
+const dialogIdEl = document.querySelector("#myDialog #dialogId");
+const totalStesUl = document.querySelector("#stats-ul");
 
 let startWeight = 0;
 let myUserId = "";
 
-async function renderId(){
-let userdata = await getMyId();
-myUserId = userdata.user_id
+async function renderId() {
+  let userdata = await getMyId();
+  myUserId = userdata.user_id;
 
-console.log(userdata)}
+  
+}
 renderId();
-
-
-
 
 // Hartaan /me tiedot omakanta stats.
 async function renderStats() {
@@ -51,69 +57,60 @@ async function renderStats() {
   console.log(data);
   //myUserId = data[0].user_id;
   console.log(myUserId);
-  diaryTextField.innerHTML = '';
+  diaryTextField.innerHTML = "";
   if (!data || data.length === 0) {
-  totalStesUl.innerHTML = "<li>Ei vielä merkintöjä.</li>";
-  return;
-}
-
-
-
-// Lasketaan keskiarvo askeleille  
-let totalsteps = 0;
-data.forEach((row) => {
-  totalsteps += row.steps;
-});
-
-// Haetaan isoin paino weight kohdasta ja pienin paino weight today:
-
-let maxWeight = Number(data?.[0]?.weight ?? 0);
-let minWeight = Number(data?.[0]?.weight_today ?? 0);
-
-
-
-// Käydään läpi painotiedot
-for (let i = 1; i < data.length; i++) {
-  if (data[i].weight >maxWeight){
-    maxWeight = data[i].weight;
-    startWeight = maxWeight;
+    totalStesUl.innerHTML = "<li>Ei vielä merkintöjä.</li>";
+    return;
   }
-  if (data[i].weight_today < minWeight){
-    minWeight = data[i].weight_today;
+
+  // Lasketaan keskiarvo askeleille
+  let totalsteps = 0;
+  data.forEach((row) => {
+    totalsteps += row.steps;
+  });
+
+  // Haetaan isoin paino weight kohdasta ja pienin paino weight today:
+
+  let maxWeight = Number(data?.[0]?.weight ?? 0);
+  let minWeight = Number(data?.[0]?.weight_today ?? 0);
+
+  // Käydään läpi painotiedot
+  for (let i = 1; i < data.length; i++) {
+    if (data[i].weight > maxWeight) {
+      maxWeight = data[i].weight;
+      startWeight = maxWeight;
+    }
+    if (data[i].weight_today < minWeight) {
+      minWeight = data[i].weight_today;
+    }
   }
-}
-  //console.log(`paino11 : ${maxWeight}`)
-  //console.log(`paino tänään11: ${minWeight}`)
+  
 
   //Lasketaan painon muutos
   let weightLoss = 0;
   weightLoss = (maxWeight - minWeight).toFixed(2);
 
+  // Lasketaan keskiarvo kaikille askeleille ja montako merkintää on
+  const stepMarks = data.length;
+  const averageSteps = Math.round(totalsteps / stepMarks); //Pyöristää desimaalit kokonaisluvuksi
 
+  // Näytetään keskiarvo ja askeleiden yhteismäärä ja laitetaan html
+  totalStesUl.innerHTML = "";
+  const averageTotals = document.createElement("li");
 
-
-// Lasketaan keskiarvo kaikille askeleille ja montako merkintää on
-const stepMarks = data.length;
-const averageSteps = Math.round(totalsteps / stepMarks) //Pyöristää desimaalit kokonaisluvuksi
-
-
-// Näytetään keskiarvo ja askeleiden yhteismäärä ja laitetaan html
-totalStesUl.innerHTML="";
-const averageTotals = document.createElement("li");
-
-
-averageTotals.innerHTML = `
+  // Keskiarvojen lisääminen nettisivuille
+  averageTotals.innerHTML = `
 Askelien keskiarvo per päivä: ${averageSteps}<br>
 Asekeleet yhteensä päiväkirjan mukaan: ${totalsteps}<br>
 Paino tippunut: ${weightLoss} kiloa<br>
 Ylin paino: ${maxWeight} kiloa<br>
-Alin paino: ${minWeight} kiloa<br>`
-totalStesUl.appendChild(averageTotals);
+Alin paino: ${minWeight} kiloa<br>`;
+  totalStesUl.appendChild(averageTotals);
 
- // Käydään data läpi ja luodaan jokaisesta merkinnästä kortti, jossa on tietoa ja painike, joka avaa dialogin
+  // Käydään data läpi ja luodaan jokaisesta merkinnästä kortti, jossa on tietoa ja painike, joka avaa dialogin
   data.forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML =`
+    const li = document.createElement("li");
+    li.innerHTML = `
     Luotu:<strong>${formatDate(row.created_at)}</strong><br><br>
     Päivä: ${formatDateClock(row.entry_date)}<br>
     Syödyt: ${row.calories_eaten} kcal<br>
@@ -127,62 +124,203 @@ totalStesUl.appendChild(averageTotals);
     `;
 
     // Painike, joka avaa dialogin ja näyttää tarkemmat tiedot
-  const openBtn = document.createElement('button');
-  openBtn.type = 'button';
-  openBtn.textContent = 'Avaa tiedot';
-  openBtn.addEventListener('click', () => {
-    dialogIdEl.innerHTML = `
-    Tapahtuman numero: ${row.stat_id}<br>
-    Syödyt: ${row.calories_eaten} kcal<br>
-    Poltetut kalorit: ${row.calories_used} kcal<br>
-    Askeleet: ${row.steps} päivässä<br>
-    Tämänhetkinen paino: ${row.weight_today}<br>
-    Askeleet yhteensä: ${totalsteps}
-    Askeleiden keskiarvo: ${averageSteps}
-    `
-    myDialog.showModal();
-  });
+    const openBtn = document.createElement("button");
+    openBtn.type = "button";
+    openBtn.textContent = "Avaa tiedot";
+    // Kun klikataan auki niin hakee muokattavaan formiin arvot
+    openBtn.addEventListener("click", () => {
+      dialogIdEl.innerHTML = `
+    <form class="put-item-form">
+      <input type="hidden" name="stat_id" value="${row.stat_id}" />
 
-  li.appendChild(openBtn);
- 
+      <div style="margin-top: 10px">
+        <label for="statIdVisible">Tapahtuman Id:</label>
+        <input
+          type="text"
+          id="statIdVisible"
+          value="${row.stat_id}"
+          readonly
+        />
+
+        <label for="caloriesEatenInput">Syödyt kalorit:</label>
+        <input
+          type="text"
+          id="caloriesEatenInput"
+          name="calories_eaten"
+          placeholder="esim. 3000"
+          value="${row.calories_eaten}"
+          required
+        />
+
+        <label for="caloriesUsedInput">Kulutetut kalorit:</label>
+        <input
+          type="text"
+          id="caloriesUsedInput"
+          name="calories_used"
+          placeholder="esim. 3000"
+          value="${row.calories_used}"
+          required
+        />
+
+        <label for="stepsInput">Askeleet:</label>
+        <input
+          type="text"
+          id="stepsInput"
+          name="steps"
+          placeholder="esim. 3000"
+          value="${row.steps}"
+          required
+        />
+
+        <label for="weightTodayInput">Paino tänään:</label>
+        <input
+          type="text"
+          id="weightTodayInput"
+          name="weight_today"
+          placeholder="esim. 80"
+          value="${row.weight_today}"
+          required
+        />
+
+        <label for="weightInput">Lähtöpaino:</label>
+        <input
+          type="text"
+          id="weightInput"
+          name="weight"
+          placeholder="esim. 80"
+          value="${row.weight}"
+          required
+        />
+
+        <label for="sleepHoursInput">Uni:</label>
+        <input
+          type="text"
+          id="sleepHoursInput"
+          name="sleep_hours"
+          placeholder="esim. 8"
+          value="${row.sleep_hours}"
+          required
+        />
+
+        <label for="moodInput">Fiilis:</label>
+        <input
+          type="text"
+          id="moodInput"
+          name="mood"
+          placeholder="esim. hyvä"
+          value="${row.mood}"
+          required
+        />
+
+        <label for="notesInput">Muistiinpanot:</label>
+        <input
+          type="text"
+          id="notesInput"
+          name="notes"
+          placeholder="kirjoita muistiinpanot"
+          value="${row.notes ?? ""}"
+          required
+        />
+        
+        <label for="dateInput">Päivämäärä joka kirjattu:</label>
+        <input
+          type="date"
+          id="dateInput"
+          name="entry_date"
+          value="${row.entry_date?.slice(0, 10) ?? ""}"
+          required
+        />
+      </div>
+
+      <button type="submit" style="margin-top: 10px">Päivitä (PUT)</button>
+      <button type="button" id="deleteBtn">Poista merkintä</button>
+    </form>
+  `;
+
+      const putItemForm = dialogIdEl.querySelector(".put-item-form");
+
+      // Valitun merkinnän poistaminen
+      const deleteButton = document.querySelector("#deleteBtn");
+      deleteButton.addEventListener("click", async (event) => {
+        event.preventDefault();
+        const sure = confirm("Haluatko varmasti poistaa merkinnän?");
+
+        if (!sure) {
+          return;
+        }
+
+        console.log(row.stat_id);
+        const body = {
+          stat_id: row.stat_id,
+        }; //Ilmoitus poistosta, lähetetään tiedot ja sulketaan dialogi
+        alert("Päiväkirja merkintä poistettu");
+        await deleteDailyStat(body);
+        await renderStats();
+        myDialog.close();
+      });
+
+      // Muokatun formin (Päiväkirjamerkinnän päivittäminen) lähettäminen
+putItemForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const ent1 = new FormData(putItemForm);
+  const payload = Object.fromEntries(ent1.entries());
+
+  const body = {
+    user_id: myUserId,
+    stat_id: Number(payload.stat_id),
+    calories_eaten: Number(payload.calories_eaten),
+    calories_used: Number(payload.calories_used),
+    steps: Number(payload.steps),
+    weight_today: Number(payload.weight_today),
+    mood: payload.mood,
+    weight: Number(payload.weight),
+    sleep_hours: Number(payload.sleep_hours),
+    notes: payload.notes,
+    entry_date: payload.entry_date,
+  };
+  
+  // Frontin ilmoitus toiminnasta
+  try {
+    await putDailyStats(body);
+    alert("Päiväkirjamerkintä päivitetty");
+    myDialog.close();
+    await renderStats();
+  } catch (error) {
+    alert("Päivittäminen epäonnistui");
+    console.error(error);
+  }
+});
+
+myDialog.showModal();
+    });
+
+    li.appendChild(openBtn);
     diaryTextField.appendChild(li);
   });
 }
 
-
-//diaryForm.addEventListener('submit', async (event) => {
-  //event.preventDefault();
-
-  // jos haluat tallentaa inputin tietokantaan, tee tässä POST
-  // await createDiaryEntry(diaryInput.value);
-
- // diaryInput.value = '';
-  //await renderStats(); // hae ja piirrä uudelleen
-//});
-
 await renderStats();
 
+diaryForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const ent = new FormData(diaryForm);
 
+  const payload = Object.fromEntries(ent.entries());
+  //Numerokenttien muutokset:
+  payload.steps;
+  payload.calories_eaten;
+  payload.calories_used;
+  payload.weight_today;
+  payload.sleep_hours;
+  payload.mood;
+  payload.notes;
+  payload.weight;
+  payload.entry_date;
 
- diaryForm.addEventListener('submit', async (event) => 
-  {event.preventDefault(); 
-    const ent = new FormData(diaryForm); 
-    
-    const payload = Object.fromEntries(ent.entries()); 
-   //Numerokenttien muutokset: 
-   payload.steps;
-    payload.calories_eaten
-   payload.calories_used;
-    payload.weight_today;
-    payload.sleep_hours;
-    payload.mood;
-    payload.notes;
-    payload.weight;
-    payload.entry_date;
-    
   //payload.weight = startWeight;
 
-   /** let body = {
+  /** let body = {
     "user_id":user_id,
     "calories_eaten":payload.calories_eaten,
    "calories_used":payload.calories_used,
@@ -192,24 +330,23 @@ await renderStats();
     "sleep_hours":payload.sleep_hours,
     "weight": 20,
     "notes":payload.notes,
-    }*/ 
-   let body ={
-  "user_id":myUserId,
-  "calories_eaten": Number(payload.calories_eaten),
-  "calories_used": Number(payload.calories_used),
-  "steps": Number(payload.steps),
-  "weight_today": Number(payload.weight_today),
-  "mood": payload.mood,
-  "weight": Number(payload.weight),
-  "sleep_hours": payload.sleep_hours,
-  "notes": payload.notes,
-  "entry_date": payload.entry_date,
-}
+    }*/
+  let body = {
+    user_id: myUserId,
+    calories_eaten: Number(payload.calories_eaten),
+    calories_used: Number(payload.calories_used),
+    steps: Number(payload.steps),
+    weight_today: Number(payload.weight_today),
+    mood: payload.mood,
+    weight: Number(payload.weight),
+    sleep_hours: payload.sleep_hours,
+    notes: payload.notes,
+    entry_date: payload.entry_date,
+  };
 
-    console.log(body)
+  console.log(body);
 
-  await postDailyStats(body); 
-   diaryForm.reset(); 
-   await renderStats();
-    })
-   
+  await postDailyStats(body);
+  diaryForm.reset();
+  await renderStats();
+});
